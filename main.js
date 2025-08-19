028bc0ca270d07592fa9b9077cb6a2bbbf1b4c9c
Lero <lero@example.com>
2025-08-19 10:13:21 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, "icon.png"), // 🌸 هنا الايقونة
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // 🔥 إلغاء الـ menubar
+  win.setMenuBarVisibility(false);
+  // أو تقدر تستخدم: win.removeMenu();
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
